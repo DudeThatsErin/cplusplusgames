07ccb0ec3bd8f8219fe35fae2ea2bbb4219dd11b
Erin Skidds <dudethatserin@outlook.com>
2023-02-01 09:26:17 -0500

    Created block letters of my name and a snowman

---
+++ block_letters/README.md
@@ -0,0 +1,7 @@
+# Block Letters
+
+This is a program that displays my first name in block letters as shown below:
+
+![Block Letters](https://content.codecademy.com/courses/learn-cpp/hello-world/block-letters.png)
+
+I also created a snowman using this method.
---
+++ block_letters/block_letters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  // I love programming!
+  std::cout << "  EEEE   RRRRRR    I      NNNNNN      N\n";
+  std::cout << "  E      R    R    I      N    N      N\n";
+  std::cout << "  E      RRRRRR    I      N      N    N\n";
+  std::cout << "  EEEE   R    R    I      N       N   N\n";
+  std::cout << "  E      R     R   I      N        N  N\n";
+  std::cout << "  E      R      R  I      N         N N\n";
+  std::cout << "  EEEE   R       R I      N          NN\n";
+
+  return 0;
+}
